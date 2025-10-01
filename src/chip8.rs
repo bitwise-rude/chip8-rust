0f1f219af847830534109d5fe58eff8f7e3e2be3
bitwise-rude <arunodayamavi14@gmail.com>
2025-10-01 15:01:49 +0545

    fixed

--- src/chip8.rs
+++ src/chip8.rs
@@ -1,58 +1,85 @@
 use rand::Rng;
 
-pub struct Interpreter{
-    registers:[u8;16],
-    pc:u16, // Program counter
-    I:u16, // instruction register
-
-    memory:[u8;4096],
+pub struct Interpreter {
+    registers: [u8; 16],   // V0..VF
+    pc: u16,               // Program counter
+    I: u16,                // Index register
+    sp: u8,                // Stack pointer
+    stack: [u16; 16],      // Call stack
+    memory: [u8; 4096],    // RAM
 }
 
-impl Interpreter{
-    pub fn new(data:Vec<u8>) -> Self {
-        // defaulting all the values to 0, execpt the program counter which begins at 0x200
+impl Interpreter {
+    pub fn new(data: Vec<u8>) -> Self {
+        // Initialize memory with 0
+        let mut memory = [0; 4096];
 
-        // filling the memory with the rom contents
-        // starting at 0x200 since the previous
-        // is filled with the interpreter and not 
-        // meant for program
-        let mut memory = [0;4096];
+        // Load ROM contents into memory starting at 0x200
+        memory[0x200..(0x200 + data.len())].copy_from_slice(&data);
 
-        // for (i, val) in data.iter().enumerate() {
-        //     memory[0x200 + i] = *val;
-        // }
-
-        memory[0x200..(0x200+data.len())].copy_from_slice(&data);
-
-        Interpreter { registers:[0;16],pc:0x200,I:0,memory:memory }
-
+        Interpreter {
+            registers: [0; 16],
+            pc: 0x200, // Programs start at 0x200
+            I: 0,
+            sp: 0,
+            stack: [0; 16],
+            memory,
+        }
     }
 
+    pub fn step(&mut self) {
+        // Fetch
+        let opcode = ((self.memory[self.pc as usize] as u16) << 8)
+                   | (self.memory[(self.pc + 1) as usize] as u16);
+        self.pc += 2;
 
-    pub fn step(&mut self){
-        // steps the interpreter by executing an instruction
-        let current_byte = self.memory[self.pc as usize];
-        self.pc  += 1;
-        let next_byte = self.memory[(self.pc+1) as usize];
-        println!("CURRENT OPCODE IS {:x}{:x}",current_byte,next_byte);
+        // Decode
+        let nnn = opcode & 0x0FFF;
+        let n   = (opcode & 0x000F) as u8;
+        let x   = ((opcode & 0x0F00) >> 8) as usize;
+        let y   = ((opcode & 0x00F0) >> 4) as usize;
+        let kk  = (opcode & 0x00FF) as u8;
 
-
-
-        // matching for xnnn instructions
-        let nnn = (((current_byte & 0x0F) as u16) << 8) | next_byte as u16;
-        // for Cx types of intruction
-        let x = current_byte & 0x0F;
         let mut rng = rand::thread_rng();
 
-        match current_byte & 0xF0{
-            0xA0 => self.I = nnn, 
-            0xC0 => self.registers[x as usize] = rng.gen_range(1..=255),
-
-            _ => panic!("Not Implemented")
+        // Execute
+        match opcode & 0xF000 {
+            0x1000 => { // JP addr
+                self.pc = nnn;
+            }
+            0x2000 => { // CALL addr
+                self.stack[self.sp as usize] = self.pc;
+                self.sp += 1;
+                self.pc = nnn;
+            }
+            0x3000 => { // SE Vx, byte
+                if self.registers[x] == kk {
+                    self.pc += 2;
+                }
+            }
+            0xA000 => { // LD I, addr
+                self.I = nnn;
+            }
+            0xC000 => { // RND Vx, byte
+                self.registers[x] = rng.random::<u8>() & kk;
+            }
+            0x0000 => match opcode {
+                0x00E0 => { // CLS
+                    panic!("CLS not implemented");
+                }
+                0x00EE => { // RET
+                    self.sp -= 1;
+                    self.pc = self.stack[self.sp as usize];
+                }
+                _ => panic!("Not implemented: {:#X}", opcode),
+            },
+            _ => panic!("Unknown opcode: {:#X}", opcode),
         }
-     
-        // debugging
-        println!("Registers-{:?}\nI-{}",self.registers,self.I);
-    }
 
+        // Debugging output
+        println!(
+            "Registers: {:?}, I={}, PC={:X}, SP={}",
+            self.registers, self.I, self.pc, self.sp
+        );
+    }
 }
